7b6d2f77f9e7c7e75c683c9975bdf6a4dabbe2bb
Fachriezal Nugraha P <fachriezalnugraha@192.168.1.7>
2022-06-17 23:56:57 +0700

    Update Range Tanggal

--- app/views.py
+++ app/views.py
@@ -22,7 +22,7 @@ def main():
     Aplikasi untuk melakukan prediksi pada harga beli dan harga jual emas. Model machine learning
     yang digunakan adalah regresi linier dan regresi linier dengan optimalisasi algoritma genetika.
     Pelatihan model dilakukan dengan menggunakan dataset harga emas pada kurun waktu `1 Januari 2017`
-    hingga `31 Juli 2021`.
+    hingga `19 Mei 2022`.
 
     Adapun fitur-fitur yang terdapat pada aplikasi ini adalah:
     - Prediksi harga emas pada jangka waktu tertentu.
