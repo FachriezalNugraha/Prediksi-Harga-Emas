f815e8bfd74af70b8c0d99a5f3b67f20553c02f6
Fachriezal Nugraha <fachriezalnugraha@192.168.1.16>
2022-07-11 23:32:36 +0700

    update function

--- app/views.py
+++ app/views.py
@@ -198,13 +198,13 @@ def main():
             scaler_y = get_session("scaler_y")
 
             # Evaluasi model regresi linier
-            mse, rmse = evaluate(X_test, y_test, linreg, scaler_y)
-            linreg_metrics = [mse, rmse, None]
+            r2, mse, rmse = evaluate(X_test, y_test, linreg, scaler_y)
+            linreg_metrics = [r2, mse, rmse, None]
 
             # Evaluasi model regresi linier + GA
             mse_ga, rmse_ga = evaluate(X_test, y_test, linreg_ga, scaler_y)
             #best_fitness = 1 / mse_ga 
-            linreg_ga_metrics = [mse_ga, rmse_ga, best_fitness]
+            linreg_ga_metrics = [r2_ga, mse_ga, rmse_ga, best_fitness]
 
             st.write(f"Metrik regresi pada harga {mode}")
 
@@ -212,15 +212,16 @@ def main():
             metric_table = pd.DataFrame(
                 data=[linreg_metrics, linreg_ga_metrics],
                 index=["Regresi Linier", "Regresi Linier + GA"],
-                columns=["MSE", "RMSE", "Fitness"]
+                columns=["R2","MSE", "RMSE", "Fitness"]
             )
             metric_table = metric_table.style.format(precision=7)
             st.table(metric_table)
 
             # Simpan metrik ke dalam session
             set_session(
-                mse=mse, rmse=rmse,
-                mse_ga=mse_ga, rmse_ga=rmse_ga
+                r2=r2, mse=mse, rmse=rmse,
+                r2_ga=r2_ga, mse_ga=mse_ga, rmse_ga=rmse_ga,
+                X_test_sorted=X_test_sorted, y_test_sorted=y_test_sorted
             )
                 
 
