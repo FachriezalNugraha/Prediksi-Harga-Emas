7385749d573f9d22cda7e3c23db50ae859f9ec28
Fachriezal Nugraha <fachriezalnugraha@192.168.1.16>
2022-09-28 22:37:32 +0700

    update

--- app/views.py
+++ app/views.py
@@ -215,7 +215,7 @@ def main():
                 index=["Regresi Linier", "Regresi Linier + GA"],
                 columns=["MAPE", "MSE", "RMSE", "Fitness"]
             )
-            metric_table = metric_table.style.format(precision=0)
+            metric_table = metric_table.style.format(precision=2)
             st.table(metric_table)
 
             # Simpan metrik ke dalam session
